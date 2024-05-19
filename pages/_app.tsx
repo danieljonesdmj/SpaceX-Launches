b4f5223b01b8dab53adb36970d82564befed1f2b
Daniel Jones <danieljonesdmj@gmail.com>
2024-05-19 19:59:57 +0100

    add font and heading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,12 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className="josefin-sans-font">
+      <h1 className="page_title">SpaceX Launches</h1>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
